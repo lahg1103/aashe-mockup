9ad455509b1ee7f39707a4b5370b0301badfd7db
lahg1103 <leslie.hurtado02@utrgv.edu>
2023-11-02 01:28:36 -0500

    functional menu

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    const navlinks = document.querySelector('.nav-links');
+    const nav = document.querySelector('nav');
+    
+    let headerheight = header.clientHeight;
+    let navheight = window.innerHeight - headerheight;
 
+    const hamburger = document.getElementById('hamburger-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('menu-expanded');
+        // navlinks.classList.toggle('hide');
+        nav.classList.toggle('open');
+        header.style.height = `${headerheight}px`;
+        if (nav.classList.contains('open')) {
+            nav.style.height = `${navheight}px`;
+            nav.style.maxHeight = `${navheight}px`;
+            navlinks.classList.toggle('hide');
+            
+        }
+        else {
+            setTimeout(() => {
+                navlinks.classList.toggle('hide');
+            }, 400);
+            nav.style.maxHeight = '0';
+        }
+    });
 });
